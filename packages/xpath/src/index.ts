b09d8652cbfb83a3587550216092497f96d26d11
Lucas Az <lucas-azevedo20@outlook.com>
2022-01-12 22:04:41 -0300

    Add node-type to xpath package

--- packages/xpath/src/index.ts
+++ packages/xpath/src/index.ts
@@ -1,3 +1,5 @@
+import Node from './node-type';
+
 class Step {
     value: string;
     optimized: boolean;
---
+++ packages/xpath/src/node-type.ts
@@ -0,0 +1,11 @@
+const Node = {
+    ELEMENT_NODE: 1,
+    ATTRIBUTE_NODE: 2,
+    TEXT_NODE: 3,
+    CDATA_SECTION_NODE: 4,
+    PROCESSING_INSTRUCTION_NODE: 7,
+    COMMENT_NODE: 8,
+    DOCUMENT_NODE: 9,
+};
+
+export default Node;
